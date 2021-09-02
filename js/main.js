99df5ff3c2051a04b6e6f5df06e2ccdaa437b452
samratmoin <virus.moin@gmail.com>
2021-09-02 22:28:05 +0600

    all done

--- js/main.js
+++ js/main.js
@@ -2,18 +2,15 @@
 const searchBookInfo = () => {
   const searchInput = document.querySelector("#search-field");
   const searchText = searchInput.value;
-  //   console.log(searchText);
   searchInput.value = "";
   if (searchText.length > 0) {
     document.getElementById("spinner").classList.remove("d-none");
-    // toggleSearchResult("none");
     fetch(`https://openlibrary.org/search.json?q=${searchText}`)
       .then((res) => res.json())
       .then((data) => displayInput(data.numFound));
     fetch(`https://openlibrary.org/search.json?q=${searchText}`)
       .then((res) => res.json())
       .then((data) => displayBookInfo(data.docs.slice(0, 20)))
-
       .catch((err) => {
         errorMessage();
       });
@@ -25,28 +22,26 @@ const searchBookInfo = () => {
 
 // displaying books
 const displayBookInfo = (data) => {
-  console.log(data);
   const searchBooks = document.querySelector("#search-result");
   searchBooks.textContent = "";
   data.forEach((book) => {
-    // console.log(book);
     const div = document.createElement("div");
     div.classList.add("col");
     div.innerHTML = `
-          <img src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top img-fluid" alt="book" />
+    <div class="card card-rounded border-secondary mb-3 shadow-sm h-100">
+          <img src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top img-fluid shadow-sm rounded-3" alt="book" />
           <div class="card-body">
               <h3 class="card-title">${book.title}</h3>
               <p class="card-text text-primary"><span class="text-dark">Author : </span>${book?.author_name}</p>
               <p class="card-text"><span class="text-secondary">Publisher:</span> ${book?.publisher}</p>
               <p class="card-text"><span class="text-secondary">First Publish in </span> ${book.first_publish_year}</p>
-              
           </div>
+      </div>     
                         
     `;
     searchBooks.appendChild(div);
   });
   document.getElementById("spinner").classList.add("d-none");
-  // toggleSearchResult("block");
 };
 
 // showing error
@@ -61,13 +56,8 @@ const errorMessage = () => {
           </div>`;
 };
 
-// ==========================================================
-const toggleSearchResult = (displayStyle) => {
-  document.querySelector("#search-result").style.display = displayStyle;
-};
-
+// search result amount
 const displayInput = (data) => {
-  console.log(data);
   document.getElementById(
     "item-found"
   ).innerHTML = `<p class='text-center p-3 bg-success'><b class="text-white">${data} Result Found!</b></p>`;
